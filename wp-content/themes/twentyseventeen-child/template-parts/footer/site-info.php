3ee135450e65e49bb9ed043a6a930d937b8ac5a5
Aryan Sitaula <aryan.sitaula@yahoo.com>
2018-08-27 12:52:59 +0545

    First Commit

---
+++ wp-content/themes/twentyseventeen-child/template-parts/footer/site-info.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Displays footer site info
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+<div class="site-info">
+	<?php
+	if ( function_exists( 'the_privacy_policy_link' ) ) {
+		the_privacy_policy_link( '', '<span role="separator" aria-hidden="true"></span>' );
+	}
+	?>
+        &copy; 2018 Deependra Kshetri &nbsp;&nbsp; 
+	<a href="<?php echo esc_url( __( '', 'twentyseventeen' ) ); ?>" class="imprint">
+		<?php printf( __( ' %s', 'twentyseventeen' ), '' ); ?>
+	</a>
+</div><!-- .site-info -->
